4a946815c08dd4a0647dcc790421992d3b9920b6
Hallysson Mateus <hallysson_gato@hotmail.com>
2026-06-09 23:23:18 -0300

    feat(backend): criar interface iproductrepository para o catalogo

---
+++ Backend/Ecommerce.Api/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.Api.Domain.Entities;
+
+namespace Ecommerce.Api.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        // Retorna todos os produtos ativos
+        Task<IEnumerable<Product>> GetActiveProductsAsync();
+        // Busca um produto por ID
+        Task<Product?> GetByIdAsync(int id);
+        // Busca produtos filtrados por categoria
+        Task<IEnumerable<Product>> GetByCategoryIdAsync(int categoryId);
+        // Método para o admin usar para cadastrar uma nova bijuteria
+        Task AddAsync(Product product);
+        // Salva as alterações no banco de dados
+        Task<bool> SaveChangesAsync();
+    }
+}
