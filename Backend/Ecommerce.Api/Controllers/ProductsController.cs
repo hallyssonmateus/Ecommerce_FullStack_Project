a3b88ad0b4ca25dd353c0ae7ab79a8f9c343aacd
Hallysson Mateus <hallysson_gato@hotmail.com>
2026-06-18 01:31:36 -0300

    feat(backend): implementar productscontroller para exposicao dos endpoints

---
+++ Backend/Ecommerce.Api/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using Ecommerce.Api.Domain.Entities;
+using Ecommerce.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        // Listar todos os produtos no catálogo
+        [HttpGet]
+        public async Task<IActionResult> GetCatalog()
+        {
+            var products = await _productService.GetCatalogProductsAsync();
+            return Ok(products);
+        }
+        // Obter detalhes de um produto específico
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetDetails(int id)
+        {
+            var product = await _productService.GetProductDetailsAsync(id);
+            if (product == null)
+                return NotFound(new { message = $"Produto com ID {id} não encontrado." });
+
+            return Ok(product);
+        }
+
+        // Cadastrar um novo produto (Admin)
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Product product)
+        {
+            try
+            {
+                var success = await _productService.CreateProductAsync(product);
+                if (!success)
+                    return BadRequest(new { message = "Não foi possível salvar o produto." });
+
+                return CreatedAtAction(nameof(GetDetails), new { id = product.Id }, product);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
--- Backend/Ecommerce.Api/obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
+++ Backend/Ecommerce.Api/obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb80bd33cb198d3d9ac1ca14085c7fe91e1cfac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66eb5abdc2869a77561bd0c9e74348c7ca5861d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
